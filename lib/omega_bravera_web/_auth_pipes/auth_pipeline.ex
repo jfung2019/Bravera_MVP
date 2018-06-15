94bd4dfbb2851cc14e995c182d6f49b014ef9462
njwest <njwest9@gmail.com>
2018-06-15 16:16:31 +0800

    added auth stuff but let's see if it works d_d

---
+++ lib/omega_bravera_web/_auth_pipes/auth_pipeline.ex
@@ -0,0 +1,10 @@
+defmodule OmegaBravera.Guardian.AuthPipeline do
+  use Guardian.Plug.Pipeline, otp_app: :omega_bravera,
+  module: OmegaBravera.Guardian,
+  error_handler: OmegaBravera.AuthErrorHandler
+
+  plug Guardian.Plug.VerifySession, claims: %{"typ" => "access"}
+  plug Guardian.Plug.VerifyHeader, realm: "Bearer"
+  plug Guardian.Plug.EnsureAuthenticated
+  plug Guardian.Plug.LoadResource, allow_blank: true
+end
