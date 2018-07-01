6794e4fae123e6496f9413ad6d3bed803067d53c
njwest <njwest9@gmail.com>
2018-07-01 13:42:40 +0800

    is this it

--- config/config.exs
+++ config/config.exs
@@ -9,6 +9,15 @@ use Mix.Config
   config :omega_bravera,
     ecto_repos: [OmegaBravera.Repo]
 
+# Configures the endpoint
+config :omega_bravera, OmegaBraveraWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "iKoMsrgx4QISCepc7OH9B5FUWQ0xTtHPQr8bChKjP5Z98pwVRIn9/lMax8nflh9v",
+  render_errors: [view: OmegaBraveraWeb.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: OmegaBravera.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
