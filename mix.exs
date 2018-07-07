41c1a01042058b9af04d9aa71041dc7c4eb959b7
njwest <njwest9@gmail.com>
2018-07-07 13:29:20 +0800

    alterred no-nav layout, added to 404 and 500

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule OmegaBravera.Mixfile do
   def project do
     [
       app: :omega_bravera,
-      version: "0.2.10",
+      version: "0.2.11",
       elixir: "~> 1.6.4",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
