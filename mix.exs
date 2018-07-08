63a704da58d3d0241914536efd63dc9fa074bfec
njwest <njwest9@gmail.com>
2018-07-08 19:43:14 +0800

    bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule OmegaBravera.Mixfile do
   def project do
     [
       app: :omega_bravera,
-      version: "0.2.22",
+      version: "0.3.0",
       elixir: "~> 1.6.4",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
