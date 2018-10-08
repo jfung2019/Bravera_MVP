e162457ab5445c65d5f9359abb9750d6f25f9422
Sherief Alaa <sheriefalaa.w@gmail.com>
2018-10-08 21:54:27 +0200

    Create show for ngo challenge via show ngo

--- lib/omega_bravera_web/controllers/admin/admin_panel_challenges_controller.ex
+++ lib/omega_bravera_web/controllers/admin/admin_panel_challenges_controller.ex
@@ -7,4 +7,9 @@ defmodule OmegaBraveraWeb.AdminPanelChallengesController do
     challenges = Challenges.list_ngo_chals_preload()
     render(conn, "index.html", challenges: challenges)
   end
+
+  def show(conn, %{"slug" => slug}) do
+    ngo_chal = Challenges.get_ngo_chal_by_slug(slug, user: [:strava], ngo: [])
+    render(conn, "show.html", ngo_chal: ngo_chal)
+  end
 end
