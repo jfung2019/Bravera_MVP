360fa3c2d293829ff9d0e1c3872c39e04c2e858c
Sherief Alaa <sheriefalaa.w@gmail.com>
2018-11-24 12:56:38 +0200

    Fix route path for statement

--- lib/omega_bravera_web/router.ex
+++ lib/omega_bravera_web/router.ex
@@ -102,7 +102,7 @@ defmodule OmegaBraveraWeb.Router do
       get("/ngos/:slug", AdminPanelNGOController, :show)
       get("/ngos/:slug/edit", AdminPanelNGOController, :edit)
       put("/ngos/:slug", AdminPanelNGOController, :update)
-      get("ngo/:slug/statement", AdminPanelNGOController, :statement)
+      get("/ngo/:slug/statement", AdminPanelNGOController, :statement)
     end
   end
 
