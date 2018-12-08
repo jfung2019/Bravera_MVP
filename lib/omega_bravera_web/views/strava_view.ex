400677e932ff3f41b1d5fed0ef482bf33a7dcc02
Sherief Alaa <sheriefalaa.w@gmail.com>
2018-12-08 09:57:08 +0200

    Add strava subscription params

--- lib/omega_bravera_web/views/strava_view.ex
+++ lib/omega_bravera_web/views/strava_view.ex
@@ -2,7 +2,12 @@ defmodule OmegaBraveraWeb.StravaView do
   use OmegaBraveraWeb, :view
 
   def render("hub_challenge.json", %{hub_challenge: hub_challenge}) do
-    %{"hub.challenge" => hub_challenge}
+    %{
+      "hub.mode" => "subscribe",
+      "hub.verify_token" => "STRAVA",
+      "hub.challenge" => hub_challenge,
+
+    }
   end
 
   def render("webhook_callback.json", %{status: status}) do
