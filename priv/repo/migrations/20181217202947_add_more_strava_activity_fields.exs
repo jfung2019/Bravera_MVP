d481d4c1b28e535b5de7c79ac580a8156ec57ac6
Sherief Alaa <sheriefalaa.w@gmail.com>
2018-12-17 23:44:20 +0200

    Add missing migration file and fix cards column style.

---
+++ priv/repo/migrations/20181217202947_add_more_strava_activity_fields.exs
@@ -0,0 +1,11 @@
+defmodule OmegaBravera.Repo.Migrations.AddMoreStravaActivityFields do
+  use Ecto.Migration
+
+  def change do
+    alter table(:activities) do
+      add :average_speed, :decimal, default: 0
+      add :moving_time, :integer, default: 0
+      add :elapsed_time, :integer, default: 0
+    end
+  end
+end
