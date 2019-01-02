ce80dfcb44412e6199d152f420c4520f3390236d
Sherief Alaa <sheriefalaa.w@gmail.com>
2019-01-02 13:11:21 +0200

    #1020 Add total pledged and total secured for each challenge in leaderboard

--- lib/omega_bravera_web/controllers/ngo_controller.ex
+++ lib/omega_bravera_web/controllers/ngo_controller.ex
@@ -1,7 +1,7 @@
 defmodule OmegaBraveraWeb.NGOController do
   use OmegaBraveraWeb, :controller
 
-  alias OmegaBravera.{Fundraisers, Challenges}
+  alias OmegaBravera.{Fundraisers, Challenges, Challenges.NGOChal}
 
   def index(conn, _params) do
     ngos = Fundraisers.list_ngos()
@@ -16,8 +16,22 @@ defmodule OmegaBraveraWeb.NGOController do
 
   def leaderboard(conn, %{"ngo_slug" => slug}) do
     ngo = Fundraisers.get_ngo_by_slug(slug)
-    challenges = Challenges.get_ngo_ngo_chals(ngo)
+    challenges = Challenges.get_ngo_ngo_chals(ngo) |> add_stats()
 
     render(conn, "leaderboard.html", %{ngo: ngo, challenges: challenges})
   end
+
+  defp add_stats(challenges) do
+    Enum.map(challenges, fn challenge ->
+      challenge
+      |> Map.put(:total_secured, get_total_secured(challenge))
+      |> Map.put(:total_pledged, get_total_pledged(challenge))
+    end)
+  end
+
+  defp get_total_secured(%NGOChal{} = challenge),
+   do: get_stats(challenge) |> get_in(["total", "charged"] || 0)
+
+  defp get_total_pledged(%NGOChal{} = challenge),
+   do: get_stats(challenge) |> get_in(["total", "pending"] || 0)
 end
