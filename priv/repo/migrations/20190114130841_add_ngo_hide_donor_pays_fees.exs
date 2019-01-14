d6e04a3117fdf330f7b683a641c3bf322146e9c2
Sherief Alaa <sheriefalaa.w@gmail.com>
2019-01-14 15:41:10 +0200

    1009 add the ability to hide donor_pays_fees UI

---
+++ priv/repo/migrations/20190114130841_add_ngo_hide_donor_pays_fees.exs
@@ -0,0 +1,9 @@
+defmodule OmegaBravera.Repo.Migrations.AddNgoHideDonorPaysFees do
+  use Ecto.Migration
+
+  def change do
+    alter table(:ngos) do
+      add :hide_donor_pays_fees, :boolean, null: false, default: true
+    end
+  end
+end
