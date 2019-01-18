728624be81aa934498017ac61c6182e6960036c8
Sherief Alaa <sheriefalaa.w@gmail.com>
2019-01-18 23:12:15 +0200

    1009 Make sure that donor_pays_fees is taking effect in the NGO CSV statements

--- lib/omega_bravera_web/views/admin_panel_ngo_view.ex
+++ lib/omega_bravera_web/views/admin_panel_ngo_view.ex
@@ -1,20 +1,24 @@
 defmodule OmegaBraveraWeb.AdminPanelNGOView do
   use OmegaBraveraWeb, :view
 
-  def render_fees(%Decimal{} = amount, %Decimal{} = exchange_rate, challenge_currency, opt) do
+  alias OmegaBravera.{Money.Donation}
+
+  def render_fees(%Donation{} = donation, opt) do
     amount =
-      if challenge_currency != "hkd" do
-        Decimal.mult(exchange_rate, amount)
+      if donation.donor_pays_fees do
+        donation.amount
+        |> Decimal.mult(donation.exchange_rate)
       else
-        amount
+        donation.charged_amount
       end
 
-    bravera = Decimal.mult(Decimal.new(0.06), amount)
-    gateway_percent = Decimal.mult(Decimal.new(0.034), amount)
-    gateway_base_fee = Decimal.new(2.35)
+
+    bravera = Decimal.mult(Decimal.from_float(0.06), amount)
+    gateway_percent = Decimal.mult(Decimal.from_float(0.034), amount)
+    gateway_base_fee = Decimal.from_float(2.35)
 
     net_donation =
-      Decimal.sub(amount, gateway_base_fee)
+      Decimal.sub(donation.charged_amount, gateway_base_fee)
       |> Decimal.sub(gateway_percent)
       |> Decimal.sub(bravera)
 
