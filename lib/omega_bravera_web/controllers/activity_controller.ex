f77a9a8e13862b612e0351a73ccb339aa5db6c4a
Sherief Alaa <sheriefalaa.w@gmail.com>
2019-01-29 19:25:51 +0200

    1029 fix activities page

--- lib/omega_bravera_web/controllers/activity_controller.ex
+++ lib/omega_bravera_web/controllers/activity_controller.ex
@@ -4,7 +4,7 @@ defmodule OmegaBraveraWeb.ActivityController do
   alias OmegaBravera.Challenges
 
   def index(conn, %{"ngo_chal_slug" => slug, "ngo_slug" => ngo_slug}) do
-    challenge = Challenges.get_ngo_chal_by_slugs(ngo_slug, slug, user: [:strava], ngo: [])
+    challenge = Challenges.get_ngo_chal_by_slugs(ngo_slug, slug, user: [:strava], ngo: [], team: [:users])
     activities = Challenges.latest_activities(challenge)
 
     render(conn, "index.html", %{challenge: challenge, activities: activities})
