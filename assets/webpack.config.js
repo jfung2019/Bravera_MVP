d58fdd10963d5c396aa8cfd8195b9ccbff37bfdd
Sherief Alaa <sheriefalaa.w@gmail.com>
2019-02-24 14:51:50 +0200

    Install file-loader package to properly load font-awesome with webpack

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -39,7 +39,18 @@ module.exports = (env, options) => ({
                     "css-loader", // translates CSS into CommonJS
                     "sass-loader" // compiles Sass to CSS, using Node Sass by default
                 ]
-            }
+            },
+            {
+                test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+                use: [{
+                  loader: 'file-loader',
+                  options: {
+                    name: '[name].[ext]',
+                    outputPath: 'fonts/',    // where the fonts will go
+                    publicPath: '../fonts'       // override the default path
+                  }
+                }]
+              }
         ]
     },
     plugins: [
