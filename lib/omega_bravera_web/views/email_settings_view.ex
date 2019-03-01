f78a9e2735f25bece5adebf6a49a6edc0ed32291
Sherief Alaa <sheriefalaa.w@gmail.com>
2019-03-01 10:46:40 +0200

    1049 make sure that the main email category is unsubscribable

--- lib/omega_bravera_web/views/email_settings_view.ex
+++ lib/omega_bravera_web/views/email_settings_view.ex
@@ -1,6 +1,8 @@
 defmodule OmegaBraveraWeb.EmailSettingsView do
   use OmegaBraveraWeb, :view
 
+  alias OmegaBravera.Emails.EmailCategory
+
   def subscribed?(category_id, user_categories) do
     if Enum.member?(user_categories, category_id) do
       "checked"
@@ -11,6 +13,6 @@ defmodule OmegaBraveraWeb.EmailSettingsView do
   # This was done to avoid creating user_email_categories rows for each user.
   def default_categories(user_categories), do: Enum.empty?(user_categories)
 
-  # TODO
-  # def is_main_category?(_), do: nil
+  def readonly_main_category(%EmailCategory{title: title}) when title == "Platform Notifications", do: "disabled"
+  def readonly_main_category(_), do: ""
 end
