0995cc60f3bf3e8c7775fae83085373200fab67a
Allen Wyma <allen.wyma@gmail.com>
2019-03-07 10:49:49 +0800

    catch paths that we do not handle and render 404

--- test/omega_bravera_web/controllers/page_controller_test.exs
+++ test/omega_bravera_web/controllers/page_controller_test.exs
@@ -1,8 +1,13 @@
 defmodule OmegaBraveraWeb.PageControllerTest do
-  use OmegaBraveraWeb.ConnCase
+  use OmegaBraveraWeb.ConnCase, async: true
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
     assert html_response(conn, 200) =~ "Bravera"
+  end
+
+  test "GET really long URL returns 404", %{conn: conn} do
+    conn = get(conn, "/really/long/url")
+    assert html_response(conn, 404) =~ "You look lost, Mate."
   end
 end
