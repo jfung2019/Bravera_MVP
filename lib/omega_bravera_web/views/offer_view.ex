a0e43b2ba3c66d6caeb5415ea0875487c8fed684
Sherief Alaa <sheriefalaa.w@gmail.com>
2019-03-11 12:06:00 +0200

    1072 improve user_challenge_slug() function

--- lib/omega_bravera_web/views/offer_view.ex
+++ lib/omega_bravera_web/views/offer_view.ex
@@ -21,11 +21,14 @@ defmodule OmegaBraveraWeb.Offer.OfferView do
   def user_joined_offer_before?(_, nil), do: false
 
   def user_challenge_slug(%Offer{id: id}, %User{offer_challenges: offer_challenges}) do
-    Enum.map(offer_challenges, fn offer_challenge ->
-     if id == offer_challenge.offer_id do
-      offer_challenge.slug
-     end
-    end)
-    |> hd()
+    user_challenge_slug(id, offer_challenges)
+  end
+
+  def user_challenge_slug(id, [hd | tail]) do
+    if id == hd.offer_id do
+      hd.slug
+    else
+      user_challenge_slug(id, tail)
+    end
   end
 end
