ef0a93867037088d2dfaffca717d81c4bd44f56b
Sherief Alaa <sheriefalaa.w@gmail.com>
2019-03-14 12:11:00 +0200

    fix email substitution tag

--- lib/omega_bravera/offers/notifier.ex
+++ lib/omega_bravera/offers/notifier.ex
@@ -29,7 +29,7 @@ defmodule OmegaBravera.Offers.Notifier do
     |> Email.put_template(template_id)
     |> Email.add_substitution("-firstName-", challenge.user.firstname)
     |> Email.add_substitution("-challengeLink-", challenge_url(challenge))
-    |> Email.add_substitution("qrCode", challenge_qr_code_url(challenge))
+    |> Email.add_substitution("-qrCode-", challenge_qr_code_url(challenge))
     |> Email.put_from("admin@bravera.co", "Bravera")
     |> Email.add_bcc("admin@bravera.co")
     |> Email.add_to(challenge.user.email)
