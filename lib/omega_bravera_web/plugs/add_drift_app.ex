55dba2ef960330120c2fabd6c22a08fede146187
Sherief Alaa <sheriefalaa.w@gmail.com>
2019-03-26 12:45:14 +0200

    1090 drift app per server

--- lib/omega_bravera_web/plugs/add_drift_app.ex
+++ lib/omega_bravera_web/plugs/add_drift_app.ex
@@ -2,9 +2,20 @@ defmodule OmegaBraveraWeb.AddDriftApp do
 
   import Plug.Conn
 
-  def init(opts), do: opts
+  def init(opts) do
+    case Application.get_env(:omega_bravera, :env) do
+      :prod ->
+        Keyword.put(
+          opts,
+          :drift_id,
+          Application.get_env(:omega_bravera, :drift_id)
+        )
 
-  def call(conn, _opts) do
-    assign(conn, :add_drift?, true)
+      _ ->
+        opts
+    end
   end
+
+  def call(conn, drift_id: drift_id), do: assign(conn, :drift_id, drift_id)
+  def call(conn, _opts), do: conn
 end
