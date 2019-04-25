82c1316464f312a55c0bf038214bdcfc4988f9ad
Sherief Alaa <sheriefalaa.w@gmail.com>
2019-04-25 12:32:32 +0200

    1125 make sure that admin will logout before navigating in to user space

--- lib/omega_bravera_web/controllers/admin/admin_user_session_controller.ex
+++ lib/omega_bravera_web/controllers/admin/admin_user_session_controller.ex
@@ -3,7 +3,21 @@ defmodule OmegaBraveraWeb.AdminUserSessionController do
   alias OmegaBravera.Guardian
   alias OmegaBraveraWeb.AdminLoggedIn
 
-  def new(conn, _), do: render(conn, "new.html")
+  alias OmegaBravera.Accounts.{User, AdminUser}
+
+  def new(conn, _) do
+    case Guardian.Plug.current_resource(conn) do
+      %User{} ->
+        redirect(conn, to: page_path(conn, :index))
+
+      %AdminUser{} ->
+        redirect(conn, to: admin_user_page_path(conn, :index))
+
+      _ ->
+        render(conn, "new.html")
+    end
+
+  end
 
   def create(conn, %{"session" => %{"email" => email, "password" => pass}}) do
     case AdminLoggedIn.login_by_email_and_pass(conn, email, pass) do
