e0c8676673dc8ac759ff6f1f9cb1443ad5adf703
Sherief Alaa <sheriefalaa.w@gmail.com>
2019-05-12 13:36:14 +0200

    hotfix: unique index for offer challenge activities

---
+++ priv/repo/migrations/20190512113213_add_unique_index_for_offer_activities.exs
@@ -0,0 +1,23 @@
+defmodule OmegaBravera.Repo.Migrations.AddUniqueIndexForOfferActivities do
+  use Ecto.Migration
+  import Ecto.Query
+
+  alias OmegaBravera.{Repo, Offers.OfferChallengeActivity}
+
+  def up do
+    from(a in OfferChallengeActivity,
+      join: a2 in OfferChallengeActivity,
+      on:
+        a2.strava_id == a.strava_id and
+          a2.offer_challenge_id == a.offer_challenge_id and
+          a2.id < a.id
+    )
+    |> Repo.delete_all()
+
+    create(unique_index(:offer_challenge_activities, [:strava_id, :offer_challenge_id]))
+  end
+
+  def down do
+    drop(unique_index(:offer_challenge_activities, [:strava_id, :offer_challenge_id]))
+  end
+end
