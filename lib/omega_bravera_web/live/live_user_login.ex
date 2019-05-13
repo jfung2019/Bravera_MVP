5be76a08b9af387f426e593f90904e3e96715f3e
Sherief Alaa <sheriefalaa.w@gmail.com>
2019-05-14 01:07:42 +0200

    1073 combine login input validation and credentials validation

--- lib/omega_bravera_web/live/live_user_login.ex
+++ lib/omega_bravera_web/live/live_user_login.ex
@@ -17,27 +17,27 @@ defmodule OmegaBraveraWeb.LiveUserLogin do
 
   def render(assigns), do: OmegaBraveraWeb.LayoutView.render("unauth_nav.html", assigns)
 
-  def handle_event("validate", %{"login" => params}, socket) do
+  def handle_event("validate", %{"login" => %{"email" => email, "password" => pass} = params}, socket) do
     changeset =
       %Accounts.Login{}
       |> Accounts.Login.changeset(params)
       |> Map.put(:action, :insert)
 
-    {:noreply, assign(socket, changeset: changeset, open_modal: true, login_button_disabled?: changeset.valid?)}
-  end
+    if changeset.valid? do
+      case Accounts.email_password_auth(email, pass) do
+        {:ok, _user} ->
+          {:noreply, assign(socket, open_modal: true, login_button_disabled?: changeset.valid?)}
 
-  def handle_event("try_login", %{"login" => %{"email" => email, "password" => pass}}, socket) do
-    case Accounts.email_password_auth(email, pass) do
-      {:ok, _user} ->
-        # TODO: User login...
-        {:stop, assign(socket, open_modal: true)}
+        {:error, :invalid_password} ->
+          {:noreply, assign(socket, error: "Invalid email and password combo.", open_modal: true, login_button_disabled?: false)}
 
-      {:error, :invalid_password} ->
-        {:noreply, assign(socket, error: "Invalid email and password combo.", open_modal: true, login_button_disabled?: false)}
-
-      {:error, :user_does_not_exist} ->
-        {:noreply, assign(socket, error: "User does not exist in our database.", open_modal: true, login_button_disabled?: false)}
+        {:error, :user_does_not_exist} ->
+          {:noreply, assign(socket, error: "User does not exist in our database.", open_modal: true, login_button_disabled?: false)}
+      end
+    else
+      {:noreply, assign(socket, changeset: changeset, open_modal: true, login_button_disabled?: changeset.valid?)}
     end
+
   end
 
   def handle_event("close_modal", _, socket), do: {:noreply, assign(socket, open_modal: false)}
