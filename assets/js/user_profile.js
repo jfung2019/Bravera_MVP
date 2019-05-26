d638bdcff1146c2d1174c9a769a45d569026458e
Sherief Alaa <sheriefalaa.w@gmail.com>
2019-05-26 18:53:21 +0200

    1138 improve UX for profile image upload. Add warning if file is > 2MB and remove upload button

--- assets/js/user_profile.js
+++ assets/js/user_profile.js
@@ -6,5 +6,14 @@ $(function() {
     const fileName = $(this).val().replace(/^.*[\\\/]/, '');
     //replace the "Choose a file" label
     $(this).next('.custom-file-label').html(fileName.slice(0, 16) + "..");
+
+
+    if(this.files[0].size > 2097152){
+      $(".upload-too-large").removeClass("d-none");
+      $(".upload-now").addClass("d-none");
+    } else {
+      $(".upload-too-large").addClass("d-none");
+      $(".upload-now").removeClass("d-none");
+    }
   });
 });
