21091cec688316b39a1e50784f6deb2e07c92af6
Sherief Alaa <sheriefalaa.w@gmail.com>
2019-05-31 00:31:24 +0200

    1151 add admin offer index stats

--- lib/omega_bravera_web/views/admin_panel_offer_view.ex
+++ lib/omega_bravera_web/views/admin_panel_offer_view.ex
@@ -12,6 +12,20 @@ defmodule OmegaBraveraWeb.AdminPanelOfferView do
     datetime_select(form, field, [builder: builder] ++ opts)
   end
 
+  def completed_challenges(offer_challenges) do
+    stats =
+      Enum.reduce(offer_challenges, %{active: 0, complete: 0, expired: 0}, fn offer_challenge, acc ->
+        cond do
+          offer_challenge.status == "active" -> Map.update(acc, :active, 0, &(&1 + 1))
+          offer_challenge.status == "expired" -> Map.update(acc, :expired, 0, &(&1 + 1))
+          offer_challenge.status == "complete" -> Map.update(acc, :complete, 0, &(&1 + 1))
+          true -> acc
+        end
+      end)
+
+    "C: #{stats[:complete]} <br /> E: #{stats[:expired]} <br /> L: #{stats[:active]}"
+  end
+
   def get_total_redeems_value(offer_redeems) do
     Enum.filter(offer_redeems, fn offer_redeem ->
       not is_nil(offer_redeem.offer_reward_id) and offer_redeem.status == "redeemed"
