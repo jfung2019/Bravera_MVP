1273dc65317f811dcddc7ceafcd8a474d453ed74
Allen Wyma <allen@plangora.com>
2019-06-13 10:49:14 +0800

    redirect user to login if invalid session

--- lib/omega_bravera_web/plugs/auth_error_handler.ex
+++ lib/omega_bravera_web/plugs/auth_error_handler.ex
@@ -1,5 +1,6 @@
 defmodule OmegaBravera.AuthErrorHandler do
   alias OmegaBravera.Guardian
+  import Phoenix.Controller, only: [redirect: 2, put_flash: 3]
   require Logger
 
   # TODO handle this properly
@@ -7,10 +8,13 @@ defmodule OmegaBravera.AuthErrorHandler do
     Logger.warn("AUTH_ERROR CALLBACK: #{inspect(error_tuple)}")
     conn
     |> Guardian.Plug.sign_out()
+    |> redirect(to: "/login")
   end
 
   def auth_error(conn, {_type, _reason} = error_tuple, _opts) do
     Logger.warn("AUTH_ERROR CALLBACK: #{inspect(error_tuple)}")
     conn
+    |> put_flash(:info, "Please login")
+    |> redirect(to: "/login")
   end
 end
