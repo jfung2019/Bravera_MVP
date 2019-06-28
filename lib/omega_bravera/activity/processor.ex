134e503ccb0348fa5d04daf800667709f794e32e
Sherief Alaa <sheriefalaa.w@gmail.com>
2019-06-28 12:34:01 +0200

    1143 start ingestion modules under a supervisor

--- lib/omega_bravera/activity/processor.ex
+++ lib/omega_bravera/activity/processor.ex
@@ -1,7 +1,7 @@
 defmodule OmegaBravera.Activity.Processor do
   require Logger
 
-  alias OmegaBravera.{Repo, Accounts, Activity.Activities, Offers.OfferActivitiesIngestion, Challenges.ActivitiesIngestion}
+  alias OmegaBravera.{TaskSupervisor, Repo, Accounts, Activity.Activities, Offers.OfferActivitiesIngestion, Challenges.ActivitiesIngestion}
 
   def process_activity(%Strava.Activity{} = strava_activity, %{"owner_id" => athlete_id} = params) do
     user = Accounts.get_strava_by_athlete_id(athlete_id) |> Repo.preload(:user)
@@ -10,8 +10,8 @@ defmodule OmegaBravera.Activity.Processor do
       {:ok, activity} ->
         Logger.info("ActivityProcessor: Saved a new activity for user #{user.id}")
 
-        Task.start(OfferActivitiesIngestion, :start, [activity, params])
-        Task.start(ActivitiesIngestion, :start, [activity, params])
+        Task.Supervisor.start_child(TaskSupervisor, OfferActivitiesIngestion, :start, [activity, params], [restart: :temporary])
+        Task.Supervisor.start_child(TaskSupervisor, ActivitiesIngestion, :start, [activity, params], [restart: :temporary])
 
       {:error, reason} ->
         Logger.info("ActivityProcessor: I received a new activity from Strava but could not save it. Reason: #{inspect(reason)}")
