02ebaafbaa2a4b53e5f77850aa6dccf73ed99f2c
Sherief Alaa <sheriefalaa.w@gmail.com>
2019-07-25 09:25:11 +0200

    1142 refresh token when getting a new activity

--- lib/omega_bravera/trackers/strava_api_helpers.ex
+++ lib/omega_bravera/trackers/strava_api_helpers.ex
@@ -1,7 +1,7 @@
 defmodule OmegaBravera.Trackers.StravaApiHelpers do
   require Logger
 
-  alias OmegaBravera.{Accounts}
+  alias OmegaBravera.{Accounts, Trackers}
   alias OmegaBravera.Trackers.Strava, as: StravaTracker
 
   def process_strava_webhook(
@@ -17,8 +17,28 @@ defmodule OmegaBravera.Trackers.StravaApiHelpers do
 
   def process_strava_webhook(_), do: {:error, :webhook_not_processed}
 
-  defp get_strava_activity(%StravaTracker{token: token}, %{"object_id" => object_id}),
-   do: Strava.Activities.get_activity_by_id(Strava.Client.new(token), object_id, include_all_efforts: true)
+  defp get_strava_activity(%StravaTracker{token: token, refresh_token: refresh_token} = athlete, %{"object_id" => object_id}) do
+    client = Strava.Client.new(token,
+      refresh_token: refresh_token,
+      token_refreshed: fn client ->
+        attrs = %{
+          token: client.token.access_token,
+          refresh_token: client.token.refresh_token,
+          token_expires_at: Timex.from_unix(client.token.expires_at)
+        }
+
+        case Trackers.update_strava(athlete, attrs) do
+          {:ok, _} ->
+            Logger.info("Successfully refreshed token for strava athlete: #{athlete.firstname} #{athlete.lastname} #{athlete.athlete_id}")
+
+          {:error, reason} ->
+            Logger.warn("Failed to refresh token, reason: #{inspect(reason)}.")
+        end
+      end
+    )
+
+    Strava.Activities.get_activity_by_id(client, object_id, include_all_efforts: true)
+  end
 
   defp get_strava_activity(nil, _), do: {:error, :no_user_matching_athlete_id}
 end
