1153648541dc73491b8b09fbb87fa49dde67c9a4
Sherief Alaa <sheriefalaa.w@gmail.com>
2019-08-13 13:14:50 +0200

    1206 fix route again

--- lib/omega_bravera_web/router.ex
+++ lib/omega_bravera_web/router.ex
@@ -150,7 +150,7 @@ defmodule OmegaBraveraWeb.Router do
       get("/challenges", AdminPanelChallengesController, :index)
 
       resources("/ngos", AdminPanelNGOController, only: [:index, :new, :create]) do
-        resources("/", AdminPanelChallengesController,
+        resources("/challenges", AdminPanelChallengesController,
           only: [:show, :edit, :update],
           param: "slug"
         )
