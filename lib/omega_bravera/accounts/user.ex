5c61daaf69b1eddf3ce4527846ad6cfa855ccd8e
Allen Wyma <allen.wyma@gmail.com>
2020-05-07 11:47:20 +0800

    fix query for listing tokens for expiring offer users

--- lib/omega_bravera/accounts/user.ex
+++ lib/omega_bravera/accounts/user.ex
@@ -68,6 +68,7 @@ defmodule OmegaBravera.Accounts.User do
     has_many :subscribed_email_categories, OmegaBravera.Notifications.UserEmailCategories
     has_many :offer_challenges, OfferChallenge
     has_many :activities, OmegaBravera.Activity.ActivityAccumulator
+    has_many :offer_redeems, OmegaBravera.Offers.OfferRedeem
     belongs_to :location, OmegaBravera.Locations.Location
 
     many_to_many :teams, Team, join_through: "team_members"
