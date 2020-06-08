634053e4d2a82ca3d27ef1374d894158294b439c
Allen Wyma <allen.wyma@gmail.com>
2020-06-08 18:31:27 +0800

    remove warning for expiring offer redeems

--- lib/omega_bravera/offers/jobs/expire_offer_redeem.ex
+++ lib/omega_bravera/offers/jobs/expire_offer_redeem.ex
@@ -3,5 +3,8 @@ defmodule OmegaBravera.Offers.Jobs.ExpireOfferRedeem do
   alias OmegaBravera.Offers
 
   @impl Oban.Worker
-  def perform(_args, _job), do: Offers.expire_expired_offer_redeems()
+  def perform(_args, _job) do
+    Offers.expire_expired_offer_redeems()
+    :ok
+  end
 end
