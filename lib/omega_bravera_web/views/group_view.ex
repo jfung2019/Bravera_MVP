972141aed2306afb5059b92091d9a932bc709bf1
Allen Wyma <allen.wyma@gmail.com>
2020-11-23 16:02:22 +0800

    fix error for rendering messages

--- lib/omega_bravera_web/views/group_view.ex
+++ lib/omega_bravera_web/views/group_view.ex
@@ -11,23 +11,21 @@ defmodule OmegaBraveraWeb.GroupView do
       name: group.name,
       image: List.first(group.images),
       chat_messages:
-        Phoenix.View.render_many(group.chat_messages, __MODULE__, "show_message.json"),
+        Phoenix.View.render_many(group.chat_messages, __MODULE__, "show_message.json", as: :message),
       users: Phoenix.View.render_many(group.users, __MODULE__, "show_user.json", as: :user)
     }
   end
 
   def render("show_message.json", %{message: message}) do
-    user = message.user
-
     %{
       id: message.id,
-      user: %{id: user.id, username: user.username, profile_picture: user.profile_picture},
+      user: Phoenix.View.render_one(message.user, __MODULE__, "show_user.json", as: :user),
       group_id: message.group_id,
       message: message.message
     }
   end
 
   def render("show_user.json", %{user: user}) do
-    %{id: user.id, username: user.username}
+    %{id: user.id, username: user.username, profile_picture: user.profile_picture}
   end
 end
