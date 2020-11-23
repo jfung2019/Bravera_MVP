ebef1bb3178fc78cf8de08ff4e3ba370311385d3
Allen Wyma <allen.wyma@gmail.com>
2020-11-23 18:53:58 +0800

    add in message meta data type and message inserted_at

--- lib/omega_bravera_web/views/group_view.ex
+++ lib/omega_bravera_web/views/group_view.ex
@@ -24,8 +24,13 @@ defmodule OmegaBraveraWeb.GroupView do
       user: Phoenix.View.render_one(message.user, __MODULE__, "show_user.json", as: :user),
       group_id: message.group_id,
       message: message.message,
-      meta_data: %{likes: message.meta_data.likes, emoji: message.meta_data.emoji}
+      meta_data: Phoenix.View.render_one(message.meta_data, __MODULE__, "show_meta_data.json", as: :meta_data),
+      inserted_at: message.inserted_at
     }
+  end
+
+  def render("show_meta_data.json", %{meta_data: meta_data}) do
+   %{likes: meta_data.likes, emoji: meta_data.emoji, message_type: meta_data.message_type}
   end
 
   def render("show_user.json", %{user: user}) do
