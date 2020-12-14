f4cfa49bb42ef7a0c104663575b6d5b0eb7846fc
Allen Wyma <allen.wyma@gmail.com>
2020-12-14 10:44:00 +0800

    log error assigns for 500 error request

--- lib/omega_bravera_web/views/error_view.ex
+++ lib/omega_bravera_web/views/error_view.ex
@@ -1,7 +1,11 @@
 defmodule OmegaBraveraWeb.ErrorView do
   use OmegaBraveraWeb, :view
+  require Logger
 
-  def render("500.json", _assigns), do: %{error: "Server error"}
+  def render("500.json", assigns) do
+    Logger.error("Error rendered: #{inspect(assigns)}")
+    %{error: "Server error"}
+  end
 
   # In case no render clause matches or no
   # template is found, let's render it as 500
