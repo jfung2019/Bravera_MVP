28d09c95264b076cd04964af2c257dffcd04623f
JasonTsui <jason@plangora.com>
2020-12-31 14:10:19 +0800

    add icons and subtext

--- assets/js/init_html_editor.js
+++ assets/js/init_html_editor.js
@@ -2,6 +2,16 @@ import "trumbowyg";
 
 $(() => {
     $('textarea[data-html]').trumbowyg({
-        svgPath: '/fonts/trumbowyg/icons.svg'
+        svgPath: '/fonts/trumbowyg/icons.svg',
+        btns: [
+            ['viewHTML'],
+            ['undo', 'redo'],
+            ['formatting'],
+            ['strong', 'em', 'del'],
+            ['link'],
+            ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
+            ['horizontalRule'],
+            ['removeformat'],
+        ]
     });
 });
