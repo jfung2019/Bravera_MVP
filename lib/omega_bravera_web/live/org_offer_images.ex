7d76c4fec67e3e9732015e0c7b2cfa1ae4b20bfc
Allen Wyma <allen.wyma@gmail.com>
2021-01-27 13:23:17 +0800

    initial run at pop up for partner offer images

--- lib/omega_bravera_web/live/org_offer_images.ex
+++ lib/omega_bravera_web/live/org_offer_images.ex
@@ -26,8 +26,34 @@ defmodule OmegaBraveraWeb.OrgOfferImages do
       ) do
     index = String.to_integer(string_index)
     images = List.delete_at(images, index)
+    {:noreply, assign(socket, images: images, to_delete: nil)}
+  end
+
+  def handle_event(
+        "shift-right",
+        %{"index" => string_index},
+        %{assigns: %{images: images}} = socket
+      ) do
+    index = String.to_integer(string_index)
+    new_index = index + 1
+    images = swap_images(images, index, new_index)
     {:noreply, assign(socket, images: images)}
   end
+
+  def handle_event(
+        "shift-left",
+        %{"index" => string_index},
+        %{assigns: %{images: images}} = socket
+      ) do
+    index = String.to_integer(string_index)
+    new_index = index - 1
+    images = swap_images(images, index, new_index)
+    {:noreply, assign(socket, images: images)}
+  end
+
+  def handle_event("to-delete", %{"index" => string_index}, socket), do: {:noreply, assign(socket, to_delete: String.to_integer(string_index))}
+
+  def handle_event("undo-delete", _, socket), do: {:noreply, assign(socket, to_delete: nil)}
 
   def handle_event("save-images", _, %{assigns: %{images: images, offer: offer}} = socket) do
     case Offers.update_offer(offer, %{images: images}) do
@@ -40,5 +66,19 @@ defmodule OmegaBraveraWeb.OrgOfferImages do
             {:noreply, redirect(socket, to: Routes.org_panel_offer_reward_path(socket, :new))}
         end
     end
+  end
+
+  defp swap_images(images, _original_index, new_index) when length(images) == new_index,
+       do: images
+
+  defp swap_images(images, 0, new_index) when new_index < 0, do: images
+
+  defp swap_images(images, original_index, new_index) do
+    original_image = Enum.at(images, original_index)
+    other_image = Enum.at(images, new_index)
+
+    images
+    |> List.replace_at(new_index, original_image)
+    |> List.replace_at(original_index, other_image)
   end
 end
