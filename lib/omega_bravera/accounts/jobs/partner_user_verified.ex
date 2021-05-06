45abda33e45e21834a17ac4b07547e292031ddfa
JasonTsui <jason@plangora.com>
2021-05-06 16:43:26 +0800

    send notification to slack channel

--- lib/omega_bravera/accounts/jobs/partner_user_verified.ex
+++ lib/omega_bravera/accounts/jobs/partner_user_verified.ex
@@ -1,7 +1,9 @@
 defmodule OmegaBravera.Accounts.Jobs.PartnerUserVerified do
   use Oban.Worker, queue: :email, max_attempts: 1
 
-  alias OmegaBravera.Accounts.Notifier
+  alias OmegaBravera.Accounts.{Notifier, SlackNotifier}
+
+  @env Application.get_env(:omega_bravera, :env)
 
   @impl Oban.Worker
   def perform(%{"id" => partner_user_id}, _job) do
@@ -10,6 +12,10 @@ defmodule OmegaBravera.Accounts.Jobs.PartnerUserVerified do
 
     Notifier.notify_verified_partner_user(partner_user)
     Notifier.notify_bravera_verified_partner_user(partner_user)
+
+    if @env != :test do
+      SlackNotifier.notify_new_partner_user(partner_user)
+    end
 
     :ok
   end
