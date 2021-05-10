5d7893c696b3358d60c456078f67b448a32ec4b0
Allen Wyma <allen.wyma@gmail.com>
2021-05-10 09:48:18 +0800

    initial stab at setting runtime settings for container

--- config/prod.exs
+++ config/prod.exs
@@ -2,9 +2,7 @@ import Config
 
 config :omega_bravera, OmegaBraveraWeb.Endpoint,
   http: [port: 5000],
-  url: [host: System.fetch_env!("HOST_URL"), port: 443, scheme: "https"],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  live_view: [signing_salt: System.fetch_env!("LIVEVIEW_SIGNING_SALT")],
   server: true,
   code_reloader: false
 
@@ -26,46 +24,23 @@ config :omega_bravera, Oban,
     {"0 0 * * *", OmegaBravera.Groups.Jobs.NotifyOrgAdminNewGrpMemberJoined}
   ]
 
-config :sendgrid,
-  api_key: System.fetch_env!("SENDGRID_API_KEY")
-
-config :omega_bravera, OmegaBravera.Endpoint,
-  secret_key_base: System.fetch_env!("SECRET_KEY_BASE")
-
 # Configure your database
 config :omega_bravera, OmegaBravera.Repo,
-  url: System.fetch_env!("DATABASE_URL"),
   pool_size: 20,
   type: OmegaBravera.PostgresTypes
 
-# Strava dev config
-config :strava,
-  client_id: System.fetch_env!("STRAVA_CLIENT_ID") |> String.to_integer(),
-  client_secret: System.fetch_env!("STRAVA_CLIENT_SECRET"),
-  access_token: System.fetch_env!("STRAVA_ACCESS_TOKEN"),
-  redirect_uri: System.fetch_env!("STRAVA_REDIRECT_URI")
-
-# Stripy dev config
-config :omega_bravera, :stripe_public_key, System.fetch_env!("STRIPE_PUBLIC_KEY")
-
 config :stripy,
-  secret_key: System.fetch_env!("STRIPE_SECRET_KEY"),
   endpoint: "https://api.stripe.com/v1/"
 
 # Guardian config
 config :omega_bravera, OmegaBravera.Guardian,
-  issuer: "Bravera.co",
-  secret_key: System.fetch_env!("GUARDIAN_SECRET_KEY")
+  issuer: "Bravera.co"
 
 # S3 Bucket
 config :ex_aws,
   access_key_id: [{:system, "AWS_ACCESS_KEY_ID"}, :instance_role],
   secret_access_key: [{:system, "AWS_SECRET_ACCESS_KEY"}, :instance_role],
-  region: "ap-southeast-1"
-
-config :omega_bravera,
-  images_bucket_name: System.fetch_env!("S3_BUCKET"),
-  images_cdn_url: System.fetch_env!("CDN_URL")
+  region: "eu-west-2"
 
 config :ex_aws, :hackney_opts,
   follow_redirect: true,
@@ -90,9 +65,3 @@ config :omega_bravera,
       ]
     ]
   ]
-
-# FCM setup
-config :pigeon, :fcm,
-  fcm_default: %{
-    key: System.fetch_env!("FCM_KEY")
-  }
---
+++ config/releases.exs
@@ -0,0 +1,47 @@
+import Config
+
+config :omega_bravera, OmegaBraveraWeb.Endpoint,
+       url: [host: System.fetch_env!("HOST_URL"), port: 443, scheme: "https"],
+       live_view: [signing_salt: System.fetch_env!("LIVEVIEW_SIGNING_SALT")]
+
+config :sendgrid,
+       api_key: System.fetch_env!("SENDGRID_API_KEY")
+
+config :omega_bravera, OmegaBravera.Endpoint,
+       secret_key_base: System.fetch_env!("SECRET_KEY_BASE")
+
+# Configure your database
+config :omega_bravera, OmegaBravera.Repo,
+       url: System.fetch_env!("DATABASE_URL")
+
+# Strava dev config
+config :strava,
+       client_id: System.fetch_env!("STRAVA_CLIENT_ID") |> String.to_integer(),
+       client_secret: System.fetch_env!("STRAVA_CLIENT_SECRET"),
+       access_token: System.fetch_env!("STRAVA_ACCESS_TOKEN"),
+       redirect_uri: System.fetch_env!("STRAVA_REDIRECT_URI")
+
+# Stripy dev config
+config :omega_bravera, :stripe_public_key, System.fetch_env!("STRIPE_PUBLIC_KEY")
+
+config :stripy,
+       secret_key: System.fetch_env!("STRIPE_SECRET_KEY")
+
+# Guardian config
+config :omega_bravera, OmegaBravera.Guardian,
+       secret_key: System.fetch_env!("GUARDIAN_SECRET_KEY")
+
+# S3 Bucket
+config :ex_aws,
+       access_key_id: [{:system, "AWS_ACCESS_KEY_ID"}, :instance_role],
+       secret_access_key: [{:system, "AWS_SECRET_ACCESS_KEY"}, :instance_role]
+
+config :omega_bravera,
+       images_bucket_name: System.fetch_env!("S3_BUCKET"),
+       images_cdn_url: System.fetch_env!("CDN_URL")
+
+# FCM setup
+config :pigeon, :fcm,
+       fcm_default: %{
+         key: System.fetch_env!("FCM_KEY")
+       }
