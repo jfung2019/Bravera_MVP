3e77b01546befa574bf0c02f1f381599b48a89dc
fung lam <fung@plangora.com>
2021-11-25 10:42:59 +0800

    change filter active to only target the filter buttons instead of targeting globally

--- lib/omega_bravera_web/live/components/time_range_filters.ex
+++ lib/omega_bravera_web/live/components/time_range_filters.ex
@@ -3,7 +3,7 @@ defmodule OmegaBraveraWeb.TimeRangeFilters do
 
   def render(assigns) do
     ~L"""
-    <div class="btn-group btn-group-toggle" data-toggle="buttons">
+    <div class="btn-group btn-group-toggle filterNav" data-toggle="buttons">
       <label class="btn btn-primary rounded active" style="margin-left: 3px; margin-right: 3px;"><input type="radio" phx-click=<%="time_range_changed_"<> assigns.for %>
       phx-value-filter="week">7 Days</label>
       <label class="btn btn-primary rounded">
